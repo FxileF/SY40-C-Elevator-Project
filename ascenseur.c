73240e92d1f2055682010c61037d01b15e3400b1
FxileF <107322253+FxileF@users.noreply.github.com>
2024-03-29 10:29:35 +0100

    Uploaded files

---
+++ ascenseur.c
@@ -0,0 +1,306 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <time.h>
+#include "list.c"
+#include <pthread.h>
+
+#define CAPACITE_MAX 10
+#define FREQUENCE 5
+#define ETAGES 9
+#define INT_MAX 2147483647
+
+// Structure pour représenter l'ascenseur
+typedef struct {
+    int etage_actuel;
+    ListeUsagers* charge;
+    Usager* usagerPrisEnCharge;
+    int destination;
+} Ascenseur;
+
+
+ListeUsagers usagers;
+ListeUsagers usagers_montants;
+ListeUsagers usagers_descendants;
+Ascenseur ascenseur1;
+Ascenseur ascenseur2;
+
+
+// Function prototype for Render
+void Render(Ascenseur *ascenseur, ListeUsagers* usagers,int destination);
+
+
+
+pthread_mutex_t mutexListe = PTHREAD_MUTEX_INITIALIZER;
+
+
+Usager randomUsager() {
+    Usager usager;
+    // 1/3 
+    int etage_0 = rand() % 6;
+    if (etage_0 == 0) {
+        usager.etage_appel = 0;
+        usager.etage_destination = rand() % (ETAGES - 1) + 1;
+        return usager;
+    }else if (etage_0 == 1) {
+        usager.etage_appel = rand() % (ETAGES - 1) + 1;
+        usager.etage_destination = 0;
+        return usager;
+    }else{
+        usager.etage_appel = rand() % (ETAGES);
+        usager.etage_destination = rand() % (ETAGES);
+        while (usager.etage_appel == usager.etage_destination) {
+            usager.etage_destination = rand() % (ETAGES);
+        }
+        return usager;
+    }
+}
+
+int usagerDirection(Usager usager){
+    if((usager.etage_destination - usager.etage_appel) > 0){
+        return 1;
+    }else if((usager.etage_destination - usager.etage_appel) < 0){
+        return -1;
+    }else{
+        return 0;
+    }
+}
+
+int ascenseurDirection(Ascenseur ascenseur, int destination){
+    if((destination - ascenseur.etage_actuel) > 0){
+        return 1;
+    }else if((destination - ascenseur.etage_actuel) < 0){
+        return -1;
+    }else{
+        return 0;
+    }
+}
+
+// Fonction pour desservir un usager
+void desservirUsagers(Ascenseur* ascenseur) {
+    while (ascenseur->charge->size != 0 && ascenseur->charge->tete->usager->etage_destination == ascenseur->etage_actuel) {
+        free(ascenseur->charge->tete->usager);
+        supprimerTete(ascenseur->charge);
+    }
+}
+
+void recupererUsagersMemeDirection(Ascenseur* ascenseur, ListeUsagers* usagers, ListeUsagers* usagers_montants, ListeUsagers* usagers_descendants, int destination) {
+    ListeUsagers* liste_courante = ascenseurDirection(*ascenseur, destination) > 0 ? usagers_montants : usagers_descendants;
+
+    if (liste_courante->size == 0) {
+        return;
+    }
+
+    UsagerNode* courant = liste_courante->tete;
+    while (courant != NULL && courant->usager->etage_appel == ascenseur->etage_actuel) {
+        if (ascenseurDirection(*ascenseur, destination) * usagerDirection(*courant->usager) > 0) {
+            ajouterCroissantDestination(ascenseur->charge, courant->usager);
+        } else {
+            printf("Ignore l'usager : A=%d, D=%d\n", courant->usager->etage_appel, courant->usager->etage_destination);
+        }
+        supprimerTete(liste_courante);
+        supprimerUsager(usagers, courant->usager);
+        courant = courant->suivant;
+    }
+}
+
+
+
+// Renvoie la destination souhaitée avec un ascenseur remplis d'usagers
+int deplacerFIFO(Ascenseur *ascenseur, ListeUsagers* usagers_montants, ListeUsagers* usagers_descendants) {
+    if (ascenseur->etage_actuel == INT_MAX) {
+        return 0;  // Ascenseur inactif, ne pas se déplacer
+    }
+
+    if (ascenseur->charge->size == 0) {
+        // Si l'ascenseur est vide, choisir la destination basée sur la direction
+        return (ascenseur->etage_actuel + ascenseur->charge->tete->usager->etage_destination) / 2;
+    }
+
+    // L'ascenseur est déjà en mouvement, continuez vers la destination actuelle
+    return ascenseur->charge->tete->usager->etage_destination;
+}
+
+// fonction pour savoir a quel etage aller pour récupérer un usager en fonction de la direction de destination
+Usager* recupererFIFO(ListeUsagers* usagers){
+    if(usagers->size == 0){
+        return NULL;
+    }
+    Usager* usagerDestination = retournerElementEnTete(usagers);
+
+    return usagerDestination;
+}
+
+void processusAscenseur(Ascenseur *ascenseur, ListeUsagers* usagers, ListeUsagers* usagers_montants, ListeUsagers* usagers_descendants) {
+    
+
+    if (ascenseur->charge->size == 0) {
+        Usager* usager = recupererFIFO(usagers);
+        ascenseur->usagerPrisEnCharge = usager;
+        supprimerUsager(usagers, usager);
+        supprimerUsager(usagers_montants, usager);
+        supprimerUsager(usagers_descendants, usager);
+
+        if (usager == NULL) {
+            ascenseur->destination = ascenseur->etage_actuel;
+        } else {
+            ascenseur->destination = usager->etage_appel;
+
+            if (ascenseur->etage_actuel == ascenseur->destination) {
+                ajouterEnTete(ascenseur->charge, ascenseur->usagerPrisEnCharge);
+            }
+        }
+
+    } else {
+        ascenseur->destination = deplacerFIFO(ascenseur, usagers_montants, usagers_descendants);
+        desservirUsagers(ascenseur);
+        //recupererUsagersMemeDirection(ascenseur, usagers, usagers_montants, usagers_descendants, destination);
+    }
+
+    if (ascenseur->etage_actuel < ascenseur->destination) {
+        ascenseur->etage_actuel += 1;
+    } else if (ascenseur->etage_actuel > ascenseur->destination) {
+        ascenseur->etage_actuel += -1;
+    }
+
+    Render(ascenseur, usagers, ascenseur->destination);
+}
+
+void Render(Ascenseur *ascenseur1, Ascenseur *ascenseur2, ListeUsagers* usagers){
+    for(int y = 0;y<ETAGES + 1;y++){
+        printf("\33[2K\r");
+        printf("\033[A");
+    }
+    for(int i = 0;i<ETAGES + 1;i++){
+        
+        if(ascenseur1->destination == ETAGES-i || ascenseur2->destination == ETAGES-i){
+            printf("► | ");
+        }else if(ascenseur2->destination == ETAGES-i){
+            printf("  | ")
+        }
+
+        if(ascenseur1->etage_actuel == ETAGES-i){
+            printf("█%d ",ascenseur1->charge->size);
+        }else {
+            printf("   ");
+        }
+        
+        if(ascenseur2->etage_actuel == ETAGES-i){
+            printf("█%d ",ascenseur2->charge->size);
+        }else {
+            printf("   ");
+        }
+
+        
+        UsagerNode* curseur = usagers->tete;
+        int up = 0;
+        int down = 0;
+        for(int x = 0;x<usagers->size;x++){
+            if(curseur->usager->etage_appel == ETAGES-i){
+                up += usagerDirection(*curseur->usager) > 0 ? 1 : 0;
+                down += usagerDirection(*curseur->usager) < 0 ? 1 : 0;
+            }
+            curseur = curseur->suivant;
+        }
+        if(up == 0){
+            printf(" | ▲ -");
+        }else{
+            printf(" | ▲ %d",up);
+        }
+        if(down == 0){
+            printf(" | ▼ -");
+        }else{
+            printf(" | ▼ %d",down);
+        }
+        printf("\n");
+
+}
+}
+
+
+
+void* threadAscenseur(void* arg) {
+    Ascenseur* ascenseur = (Ascenseur*)arg;
+
+    while (1) {
+        pthread_mutex_lock(&mutexListe);
+        processusAscenseur(ascenseur, &usagers, &usagers_montants, &usagers_descendants);
+        pthread_mutex_unlock(&mutexListe);
+        sleep(1);
+    }
+
+    return NULL;
+}
+
+
+void* threadUsagersFonction(void* arg){
+    if(FREQUENCE == 0){
+        exit(0);
+    }
+    while(1){
+        pthread_mutex_lock(&mutexListe);
+
+        Usager* random_usager = (Usager*)malloc(sizeof(Usager));
+        *random_usager = randomUsager();
+        ajouterEnQueue(&usagers,random_usager);
+        if(usagerDirection > 0)
+            ajouterCroissantDestination(&usagers_montants,random_usager);
+        else
+            ajouterDecroissantDestination(&usagers_descendants,random_usager);
+        //printf("Usager : Appelle l'ascenseur à l'étage %d, vers %d\n", random_usager->etage_appel,random_usager->etage_destination);
+        //printListe(&usagers);
+        pthread_mutex_unlock(&mutexListe);
+
+        float tts = 1 + (float)rand()/INT_MAX  * FREQUENCE * 2;
+        sleep(tts);
+    }
+
+    return NULL;
+}
+
+
+int main() {
+    srand(time(NULL));
+    usagers.tete = NULL;
+    usagers.size = 0;
+    usagers_montants.tete = NULL;
+    usagers_montants.size = 0;
+    usagers_descendants.tete = NULL;
+    usagers_descendants.size = 0;
+
+
+    ListeUsagers charge;
+    charge.tete = NULL;
+    charge.size = 0;
+
+    ascenseur1.charge = &charge;
+    ascenseur1.etage_actuel = 0;
+    ascenseur1.usagerPrisEnCharge = NULL;
+    ascenseur1.destination = 0;
+
+    ascenseur2.charge = &charge;
+    ascenseur2.etage_actuel = 0;
+    ascenseur2.usagerPrisEnCharge = NULL;
+    ascenseur2.destination = 0;
+
+    pthread_t pthreadAscenseur;
+    pthread_t pthreadUsagers;
+    pthread_t pthreadAscenseur2;
+
+
+    //crétion des threads
+    pthread_create(&pthreadAscenseur, NULL, threadAscenseur, (void*)&ascenseur1);
+    pthread_create(&pthreadAscenseur2, NULL, threadAscenseur, (void*)&ascenseur2);
+    pthread_create(&pthreadUsagers, NULL, threadUsagersFonction, NULL);
+
+
+
+    //attendre que les threads se terminent
+    pthread_join(pthreadAscenseur, NULL);
+    pthread_join(pthreadUsagers,NULL);
+    pthread_join(pthreadAscenseur2,NULL);
+
+    return 0;
+}
---
+++ list.c
@@ -0,0 +1,259 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Structure pour représenter un Usager
+typedef struct {
+    int etage_appel;
+    int etage_destination;
+} Usager;
+
+typedef struct UsagerNode{
+    Usager* usager;
+    struct UsagerNode* suivant;  // Pointeur vers le prochain Usager dans la liste
+}UsagerNode;
+
+// Liste chaînée d'Usagers
+typedef struct {
+    UsagerNode* tete;
+    int size;
+}ListeUsagers;
+
+// Fonction pour créer un nouvel Usager
+UsagerNode* creerUsager(Usager* usager) {
+    UsagerNode* nouvelUsager = (UsagerNode*)malloc(sizeof(UsagerNode));
+    nouvelUsager->usager = usager;
+    nouvelUsager->suivant = NULL;
+    return nouvelUsager;
+}
+
+void supprimerTete(ListeUsagers* liste){
+    if(liste->tete == NULL){
+        return;
+    }
+    UsagerNode* nouvelle_tete = liste->tete->suivant;
+    free(liste->tete);
+    liste->tete = nouvelle_tete;
+    liste->size--;
+}
+
+
+void supprimerUsager(ListeUsagers* liste, Usager *usager) {
+    // Cas particulier : si la liste est vide
+    if (liste->tete == NULL)
+        return;
+
+    // Cas particulier : si la valeur à supprimer est dans le premier nœud
+    if (liste->tete->usager == usager) {
+        UsagerNode* temp = liste->tete;
+        liste->tete = liste->tete->suivant;
+        free(temp);
+        liste->size--;
+        return;
+    }
+
+    // Parcours de la liste pour trouver le nœud contenant la valeur à supprimer
+    UsagerNode* current = liste->tete;
+    UsagerNode* prev = NULL;
+    while (current != NULL && current->usager != usager) {
+        prev = current;
+        current = current->suivant;
+    }
+
+    // Si la valeur n'est pas présente dans la liste
+    if (current == NULL)
+        return;
+
+    // Suppression du nœud contenant la valeur
+    prev->suivant = current->suivant;
+    free(current);
+    liste->size--;
+}
+
+
+// Fonction pour ajouter un Usager en tête de la liste
+void ajouterEnTete(ListeUsagers* liste, Usager* nouvelUsager) {
+    UsagerNode* node = creerUsager(nouvelUsager);
+    node->suivant = liste->tete;
+    liste->tete = node;
+    liste->size++;
+}
+
+// Fonction pour ajouter un élément en queue de la liste
+void ajouterEnQueue(ListeUsagers* liste, Usager* nouvelUsager) {
+    UsagerNode* node = creerUsager(nouvelUsager);  // Supposons que creerUsagerNode est une fonction qui crée un nouveau noeud
+    if (liste->tete == NULL) {
+        // Si la liste est vide, le nouvel élément devient la tête de la liste
+        liste->tete = node;
+    } else {
+        // Sinon, parcourir la liste jusqu'à la fin et ajouter le nouvel élément en queue
+        UsagerNode* courant = liste->tete;
+        while (courant->suivant != NULL) {
+            courant = courant->suivant;
+        }
+        courant->suivant = node;
+    }
+    liste->size++;
+}
+
+
+// Fonction pour parcourir la liste et afficher les éléments
+void printListe(ListeUsagers* liste) {
+    if(liste->size == 0)
+        printf("liste vide");
+    UsagerNode* courant = liste->tete;
+    while (courant != NULL) {
+        printf("A : %d , D : %d || ", courant->usager->etage_appel, courant->usager->etage_destination);
+        courant = courant->suivant;
+    }
+    printf("\n");
+}
+
+// Fonction pour retourner l'élément en tête de la liste
+Usager* retournerElementEnTete(ListeUsagers* liste) {
+    if(liste->size == 0){
+        return NULL;
+    }
+    return liste->tete->usager;
+}
+
+// Fonction pour retourner l'élément en queue de la liste
+Usager* retournerElementEnQueue(ListeUsagers* liste) {
+    UsagerNode* courant = liste->tete;
+    while (courant != NULL && courant->suivant != NULL) {
+        courant = courant->suivant;
+    }
+    return courant->usager;
+}
+
+void libererListe(ListeUsagers* liste){
+    UsagerNode* courant = liste->tete;
+    UsagerNode* suivant = NULL;
+    while (courant != NULL) {
+        suivant = courant->suivant;
+        free(courant->usager);
+        free(courant);
+        
+        courant = suivant;
+    }
+}
+
+
+void ajouterCroissantDestination(ListeUsagers* liste, Usager* nouvelUsager) {
+    UsagerNode* node = creerUsager(nouvelUsager);
+    UsagerNode* courant = liste->tete;
+    UsagerNode* precedent = NULL;
+    while (courant != NULL && courant->usager->etage_destination < nouvelUsager->etage_destination) {
+        precedent = courant;
+        courant = courant->suivant;
+    }
+    if (precedent == NULL) {
+        node->suivant = liste->tete;
+        liste->tete = node;
+    }
+    else {
+        precedent->suivant = node;
+        node->suivant = courant;
+    }
+    liste->size++;
+}
+
+void ajouterDecroissantDestination(ListeUsagers* liste, Usager* nouvelUsager) {
+    UsagerNode* node = creerUsager(nouvelUsager);
+    UsagerNode* courant = liste->tete;
+    UsagerNode* precedent = NULL;
+    while (courant != NULL && courant->usager->etage_destination > nouvelUsager->etage_destination) {
+        precedent = courant;
+        courant = courant->suivant;
+    }
+    if (precedent == NULL) {
+        node->suivant = liste->tete;
+        liste->tete = node;
+    }
+    else {
+        precedent->suivant = node;
+        node->suivant = courant;
+    }
+    liste->size++;
+}
+
+
+void ajouterCroissantAppel(ListeUsagers* liste, Usager* nouvelUsager) {
+    UsagerNode* node = creerUsager(nouvelUsager);
+    UsagerNode* courant = liste->tete;
+    UsagerNode* precedent = NULL;
+    while (courant != NULL && courant->usager->etage_appel < nouvelUsager->etage_appel) {
+        precedent = courant;
+        courant = courant->suivant;
+    }
+    if (precedent == NULL) {
+        node->suivant = liste->tete;
+        liste->tete = node;
+    }
+    else {
+        precedent->suivant = node;
+        node->suivant = courant;
+    }
+    liste->size++;
+}
+
+void ajouterDecroissantAppel(ListeUsagers* liste, Usager* nouvelUsager) {
+    UsagerNode* node = creerUsager(nouvelUsager);
+    UsagerNode* courant = liste->tete;
+    UsagerNode* precedent = NULL;
+    while (courant != NULL && courant->usager->etage_appel > nouvelUsager->etage_appel) {
+        precedent = courant;
+        courant = courant->suivant;
+    }
+    if (precedent == NULL) {
+        node->suivant = liste->tete;
+        liste->tete = node;
+    }
+    else {
+        precedent->suivant = node;
+        node->suivant = courant;
+    }
+    liste->size++;
+}
+
+/*
+int main() {
+    // Initialisation d'une liste vide
+    ListeUsagers liste;
+    ListeUsagers liste2;
+    liste.tete = NULL;
+    liste2.tete = NULL;
+
+    // Ajout d'usagers
+    Usager usager1 = {1,2};
+
+    ajouterEnTete(&liste, &usager1);
+    ajouterEnTete(&liste2, &usager1);
+
+    Usager usager2 = {1,4};
+    ajouterEnTete(&liste, &usager2);
+
+    Usager usager3 = {7,9};
+    ajouterEnTete(&liste, &usager3);
+
+    printListe(&liste);
+
+
+    Usager usager4 = {7,6};
+    ajouterDecroissantDestination(&liste, &usager4);
+
+    printListe(&liste);
+    printListe(&liste2);
+    supprimerUsager(&liste,&usager1);
+    supprimerUsager(&liste2,&usager1);
+    printListe(&liste);
+    printListe(&liste2);
+    
+
+
+    // Libération de la mémoire (à faire à la fin du programme)
+    
+
+    return 0;
+}
+*/
